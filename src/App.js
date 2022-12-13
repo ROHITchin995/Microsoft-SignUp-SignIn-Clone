60af1f4b2408ad9187d4d48b4e461207a1e78558
ROHITchin995 <rohitsure.9915@gmail.com>
2022-12-13 21:48:49 +0530

    code merged into app.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,58 @@ import { useState } from 'react';
 import './App.css';
 import Signin from './Components/SignIn';
 import SignUp from './Components/SignUp';
-
+import logo from './logo2.jpg'
 function App() {
-  const [showSignIn, setshowSignIn] = useState(true)
+  const [showSignIn, setshowSignIn] = useState(false)
 
   return (
     <div>
       {showSignIn?(
       //  sign In component
-       <Signin />  
+      <div className='main-container'>
+        <div className="inside-container">
+        <img alt="logo" className='logo' src={logo} />
+       
+       <div className="signInFont"> Sign In</div> 
+       <form action="" >
+        {/* input for email */}
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" placeholder='Email'/>
+        {/* input for password  */}
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" placeholder='Password'/>
+        <span> New User ?  <span className='formChanger'>Create account</span>  
+            </span>
+        <button>Continue</button>
+       </form>
+       </div>
+    </div> 
       ):(
         // sign Up component
-        <SignUp />
+        <div className='main-container'>
+        <div className="inside-container">
+        <img alt="logo" className='logo' src={logo} />
+        
+       <div className="signInFont"> Create Account</div> 
+       <form action="" >
+        {/* input for Name */}
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" placeholder='Name'/>
+        {/* input for email */}
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" placeholder='Email'/>
+        {/* input for password  */}
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" placeholder='Password'/>
+        {/* input for confirm Password  */}
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input type="password" name="confirmPassword" id="confirmPassword" placeholder='Confirm Password'/>
+        <span> Account already exist ? <span className='formChanger'> Continue </span>
+        </span>
+        <button>Create</button>
+       </form>
+       </div>
+    </div>
       )}
     </div>
   );
